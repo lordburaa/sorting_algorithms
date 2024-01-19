294649a992782b205e0a7d867280b25fbb73be5d
lordburaa <comrdebiruk97@gmail.com>
2024-01-19 02:14:16 -0800

    0-bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+/**
+ * swap_ints - swap tow integers in an array
+ * @a: the first integer to swap
+ * @b: the second integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * bubble_sort - srot an arry in ascending ordr
+ * @array: an array of integers to sort
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	int bubbly = 0;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == 0)
+	{
+		bubbly = 1;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = 0;
+			}
+		}
+	}
+}
