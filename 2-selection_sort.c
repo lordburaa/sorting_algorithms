714c15bcb36da12065d58e63ee269162e6155e43
lordburaa <comrdebiruk97@gmail.com>
2024-01-23 04:24:15 -0800

    secon fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,13 +9,17 @@ void selection_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
-	for(i = 0; i < size -1; i++)
+	for(i = 0; i < size -2; i++)
 	{
 		imin = i;
 		for(j = i + 1; j < size; j++)
 		{
 			if(array[j] < array[imin])
+			{
 				imin = j;
+				
+			}
+
 		}
 		temp = array[i];
 		array[i] = array[imin];
