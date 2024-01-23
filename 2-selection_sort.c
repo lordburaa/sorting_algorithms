6704012da4982003546a22860e10e86390b36f90
lordburaa <comrdebiruk97@gmail.com>
2024-01-23 11:22:08 -0800

    fix 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,22 +8,29 @@ void selection_sort(int *array, size_t size)
 	int imin;
 	int temp;
 	size_t i, j;
+	int flag;
 
-	for(i = 0; i < size -2; i++)
+	for(i = 0; i < size -1; i++)
 	{
 		imin = i;
+		flag = 0;
 		for(j = i + 1; j < size; j++)
 		{
 			if(array[j] < array[imin])
 			{
 				imin = j;
+				flag = 1;
 				
 			}
 
 		}
-		temp = array[i];
-		array[i] = array[imin];
-		array[imin] = temp;
-		print_array(array, size);
+		if (flag)
+		{
+			temp = array[i];
+			array[i] = array[imin];
+			array[imin] = temp;
+		
+			print_array(array, size);
+		}
 	}
 }	
